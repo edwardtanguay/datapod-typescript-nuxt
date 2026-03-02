d1ea94ec52d86b9fecac80bab72116b837be9eb6
Edward Tanguay (windows) <edward@tanguay.info>
2026-03-02 21:55:05 +0100

    ..clear in function

--- cli/commands/clear-debug-file.ts
+++ cli/commands/clear-debug-file.ts
@@ -1,21 +1,3 @@
-import * as qfil from "../qtools/qfil";
+import * as qdev from "../qtools/qdev";
 
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Datapod Debug Output</title>
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-	<h1>Datapod Debug Output</h1>
-	<!-- marker:bottom-of-body -->
-</body>
-</html>
-
-`;
-
-qfil.writeToFile("~~/debug/output.html", html);	
+qdev.clearDebug();
--- cli/qtools/qdev.ts
+++ cli/qtools/qdev.ts
@@ -1,5 +1,26 @@
 import * as qfil from "./qfil";
 
+export const clearDebug = (): void => {
+	const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datapod Debug Output</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<h1>Datapod Debug Output</h1>
+	<!-- marker:bottom-of-body -->
+</body>
+</html>
+
+`;
+	qfil.writeToFile("~~/debug/output.html", html);
+};
+
 export const addToDebugHtml = (content: string): void => {
 	qfil.addToTextFileBeforeMarker("~~/debug/output.html", content, "<!-- marker:bottom-of-body -->");
 };
