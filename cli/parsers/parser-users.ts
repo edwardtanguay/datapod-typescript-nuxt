f3015aea8fd0b3e1d8a8b8bc893b126a4ec58214
Edward Tanguay (windows) <edward@tanguay.info>
2026-03-03 07:04:04 +0100

    ..wrapping works

--- cli/parsers/parser-users.ts
+++ cli/parsers/parser-users.ts
@@ -1,9 +1,11 @@
 import { DpodFile } from "../classes/DpodFile";
+import { DpodItems } from "../classes/DpodItems";
 import * as qdev from "../qtools/qdev";
 
 export const parseUsers = () => {
 	const dpodFile = new DpodFile("~~/data/users.dp.txt");
+	const dpodItems = new DpodItems(dpodFile);
 
 	qdev.clearDebug();
-	qdev.addToDebugHtml(dpodFile.debugHtml());
+	dpodItems.debugHtml();
 };
